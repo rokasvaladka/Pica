5c63d8ba822ec0b211766819c7b8ed16d65e3c1f
Rokas-PC\Rokas <valadka.rokas@gmail.com>
2017-07-18 18:16:35 +0300

    all done jejeje

---
+++ javascript/script.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Rokas on 2017.07.18.
+ */
+var divs = ["Pizza", "Salad", "Starter"];
+var visibleDivId = null;
+
+function toggleVisibility(divId) {
+    if(visibleDivId === divId) {
+        //visibleDivId = null;
+    } else {
+        visibleDivId = divId;
+    }
+    hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+    var i, divId, div;
+    for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+            div.style.display = "block";
+            document.getElementById("link" + divId).classList.add("selected");
+        } else {
+            div.style.display = "none";
+            document.getElementById("link" + divId).classList.remove("selected");
+        }
+    }
+}
+function initMap() {
+    var uluru = {lat: 41.878110, lng: -87.632785};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: uluru,
+        scrollwheel: false
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
